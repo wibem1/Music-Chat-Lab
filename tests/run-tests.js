7485fb8dcc61fd93aa1c664b8ab6a37222a23ddd
wibem1 <wibem1@googlemail.com>
2026-09-16 10:56:37 +0200

    Add consolidation test runner

---
+++ tests/run-tests.js
@@ -0,0 +1,8 @@
+const {spawnSync}=require('child_process');
+const path=require('path');
+const tests=['provider-gateway.test.js','request-architecture.test.js'];
+for(const test of tests){
+  const r=spawnSync(process.execPath,[path.join(__dirname,test)],{stdio:'inherit'});
+  if(r.status!==0)process.exit(r.status||1);
+}
+console.log('CONSOLIDATION_TESTS_OK');
