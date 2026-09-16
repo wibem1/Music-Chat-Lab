69cb953a07e12f070e037b1d5c1589f8c1c95df7
wibem1 <wibem1@googlemail.com>
2026-09-16 09:34:46 +0200

    Set PWA cache build to v1.3.20

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'music-chat-lab-v1.3.19';
+const CACHE_NAME = 'music-chat-lab-v1.3.20';
 const APP_SHELL = [
   './', './index.html', './styles.css', './app.js', './music-file-processing.js',
   './midi-player.js', './midi-export.js', './midi-context.js', './midi-memory.js',
