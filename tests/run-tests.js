cadad79d189863677d44516b8538269a6bf0a1c2
wibem1 <wibem1@googlemail.com>
2026-09-16 10:58:23 +0200

    Run provider policy tests

--- tests/run-tests.js
+++ tests/run-tests.js
@@ -1,6 +1,6 @@
 const {spawnSync}=require('child_process');
 const path=require('path');
-const tests=['provider-gateway.test.js','session-request.test.js','request-architecture.test.js'];
+const tests=['provider-gateway.test.js','provider-policy.test.js','session-request.test.js','request-architecture.test.js'];
 for(const test of tests){
   const r=spawnSync(process.execPath,[path.join(__dirname,test)],{stdio:'inherit'});
   if(r.status!==0)process.exit(r.status||1);
