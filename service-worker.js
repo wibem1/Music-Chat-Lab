2a5fcaffa7bd6b60d263779e9209816bbe219742
wibem1 <wibem1@googlemail.com>
2026-09-19 05:39:46 +0200

    Update cache for v1.4.16

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'music-chat-lab-v1.4.15';
+const CACHE_NAME = 'music-chat-lab-v1.4.16';
 const APP_SHELL = [
   './', './index.html', './styles.css', './app.js', './settings-store.js', './state-vault.js', './music-file-processing.js',
   './midi-player.js', './midi-export.js', './midi-context.js', './midi-memory.js',
